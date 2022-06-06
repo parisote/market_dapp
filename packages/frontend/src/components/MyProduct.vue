6f14d2292ad4500b4233c4cbe244af40d05c4a98
Juan Martinez <jmartinez@soluciones.com.ar>
2022-06-05 21:53:55 -0300

    ver mis reservas

--- packages/frontend/src/components/MyProduct.vue
+++ packages/frontend/src/components/MyProduct.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        <h2>
-            {{ this.address }}
-        </h2>
-    </div>
+   <div class="container-fluid">
+     <div class="listContainer">
+        <Card
+          v-for="item in items"
+          :key="item.category"
+          :category="item.category"
+          :nombre="item.title"
+          :descripcion="item.descripcion"
+        />
+      </div>
+    </div>  
 </template>
 
 <script>
+import Card from "@/components/Card.vue";
 import { useStore } from '../store/store.js';
 import { storeToRefs } from 'pinia';
 
@@ -21,15 +28,35 @@ export default {
     };
   },
   data(){
-      return this.address;
+      return { items: [] };
   },
   async mounted(){
-    console.log(this.contract)
-    console.log(await this.contract.getMyPlaces())
+    
+    const result = await this.contract.getMyPlaces()
+
+    for (let i = 0; i < result.length; i++) {
+      this.items.push({
+        category: result[i].category,
+        title: result[i].title,
+        descripcion: result[i].description
+      });
+    }
+    return this.items;
   }
 };
 </script>
 
-<style>
-
+<style scoped>
+.fixCol {
+  width: 20%;
+  height: 20%;
+}
+.listContainer {
+  columns: 3 400px;
+  max-width: 1600px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  padding: 8px;
+}
 </style>
