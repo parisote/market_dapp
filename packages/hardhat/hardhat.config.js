1217b7946e074028ca497030844a3837e3c9230c
parisote <detomas@gmail.com>
2022-06-17 17:02:35 -0300

    fix: security

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -17,23 +17,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const GOERLI_PRIVATE_KEY = "4dc8d11b9023de456fabe7c98289b1328d40cc173721b944c775d540da034f3b";
+const GOERLI_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     goerli: {
       url: "https://eth-goerli.alchemyapi.io/v2/6I8_ORnqsNDUIyf0BHZKjhHsfeM4Wa5T",
-      accounts: ["4dc8d11b9023de456fabe7c98289b1328d40cc173721b944c775d540da034f3b"],
+      accounts: [GOERLI_PRIVATE_KEY],
       gasPrice: 8000000000
     },
     matic: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/6YR8gYUXJwyfdtNjTEzkQl8iiblI7kZA",//"https://rpc-mumbai.maticvigil.com",
-      accounts: ["4dc8d11b9023de456fabe7c98289b1328d40cc173721b944c775d540da034f3b"]
+      url: "https://polygon-mumbai.g.alchemy.com/v2/6YR8gYUXJwyfdtNjTEzkQl8iiblI7kZA",
+      accounts: [GOERLI_PRIVATE_KEY]
     },
     maticDapp: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/R5RrRZmPWPH6ujD4lJFRxWvS7K1jiopY",//"https://rpc-mumbai.maticvigil.com",
-      accounts: ["4dc8d11b9023de456fabe7c98289b1328d40cc173721b944c775d540da034f3b"]
+      url: "https://polygon-mumbai.g.alchemy.com/v2/R5RrRZmPWPH6ujD4lJFRxWvS7K1jiopY",
+      accounts: [GOERLI_PRIVATE_KEY]
     }
   },
 };
