457c36ae66479614e0dd1b4b91ff08501c347756
FedeJamus <fedejamus@gmail.com>
2022-06-17 17:07:59 -0300

    agrego about us

--- packages/frontend/src/components/AboutUs.vue
+++ packages/frontend/src/components/AboutUs.vue
@@ -21,10 +21,22 @@
             </div>
         </center>
     </div>
-
-    <div class="pageitem">
-        <p>Phasellus ac leo eget felis egestas vestibulum nec at velit. Vivamus venenatis, nibh ut tempus viverra, tellus augue pulvinar sapien, at iaculis justo nisi non metus. Quisque quis malesuada arcu, sed ultricies nibh. Duis vehicula metus quis arcu rutrum faucibus eget ut ex. Curabitur lacus justo, iaculis congue suscipit quis, posuere at turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec in quam non urna volutpat consequat. Proin mollis ut mauris et venenatis. Vivamus dui nibh, blandit vitae pellentesque sed, finibus sit amet nulla.</p>
-    </div>
+    <section>
+        <h1>Red ETHERIUM</h1>
+        <div class="content">
+            <p>El proyecto corre sobre la RED ETHEREUM permitiendo a los usuarios utilizar billeteras virtuales</p><a href="">Learn more</a>
+        </div>
+        <div class="img">
+            <img src="https://previews.123rf.com/images/rastudio/rastudio1712/rastudio171200888/92046169-s%C3%ADmbolo-de-ethereum-en-fondo-futurista-del-pol%C3%ADgono-del-hud-con-el-mapa-del-mundo-y-la-red-par-a-cua.jpg"/></div>    
+    </section>
+    <section>
+        <h1>VUE JS</h1>
+        <div class="content">
+            <p>Utilizamos toda la programcion con VUE JS, junto con codigo javascript y las rutas correspondientes</p><a href="">Learn more</a>
+        </div>
+        <div class="img">
+            <img src="https://lenguajejs.com/vuejs/introduccion/primeros-pasos/vue-main.js-app.vue.png"/></div>    
+    </section>
 </template>
 
 <script>
@@ -34,6 +46,7 @@ export default {
 </script>
 
 <style >
+
 .foto1 { padding: 10px; margin: 10px; border: 2px solid black; float: center; width: 250px; }
 
 .imga1 {
@@ -110,4 +123,133 @@ ul#bottomnav li {
     display:block;
     text-align:center;
 }
+:root {
+  --box-x-padding: 2rem;
+  --box-y-padding: 2rem;
+}
+@media (min-width: 40em) {
+  :root {
+    --box-y-padding: 4rem;
+  }
+}
+@media (min-width: 60em) {
+  :root {
+    --box-x-padding: 3rem;
+    --box-y-padding: 6rem;
+  }
+}
+@media (min-width: 40em) {
+  section {
+    display: grid;
+    grid-template-rows: repeat(3, auto);
+    grid-template-columns: 4fr 4fr 5fr;
+    grid-template-areas: '. . title' 'img img content' 'img img .';
+    max-width: 1000px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  section::after {
+    content: '';
+    grid-column: 2/-1;
+    grid-row: 1/3;
+    position: relative;
+    z-index: -1;
+    background: #fff;
+    box-shadow: 0 0.2em 1em rgba(85, 50, 50, 0.2);
+  }
+  h1 {
+    grid-area: title;
+    padding-top: var(--box-y-padding);
+    padding-left: var(--box-x-padding);
+  }
+  .content {
+    grid-area: content;
+    padding-bottom: var(--box-y-padding);
+    padding-left: var(--box-x-padding);
+    padding-right: var(--box-x-padding);
+  }
+  .img {
+    grid-area: img;
+  }
+}
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,400i,700");
+html {
+  font-family: Montserrat, sans-serif;
+  font-size: 15px;
+  background-color: white;
+}
+body {
+  padding: 1em;
+}
+h1 {
+  font-size: 4em;
+  margin: 0 0 1rem;
+  font-family: serif;
+  position: relative;
+}
+h1::after {
+  content: '';
+  position: absolute;
+  width: 6rem;
+  /* background-color: rgb(247, 243, 243); */
+  height: 2px;
+  top: 0;
+  left: 0;
+}
+@media (min-width: 40em) {
+  h1::after {
+    left: var(--box-x-padding);
+    top: var(--box-y-padding);
+  }
+}
+.content {
+  color: #4d4d4d;
+  margin-bottom: var(--box-y-padding);
+}
+@media (min-width: 40em) {
+  .content {
+    margin-bottom: 0;
+  }
+}
+p {
+  margin-top: 0;
+  margin-bottom: var(--box-y-padding);
+  line-height: 1.5;
+}
+.content a {
+  text-decoration: none;
+  border: 1px solid #b3b3b3;
+  border-radius: 1em;
+  padding: 1em 2em;
+  text-transform: uppercase;
+  font-size: 11px;
+  font-weight: 700;
+  letter-spacing: 0.05em;
+  color: inherit;
+  display: inline-block;
+  background-color: #fff;
+}
+.img {
+  position: relative;
+}
+.img a {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+}
+img {
+  max-width: 100%;
+  vertical-align: middle;
+  box-shadow: 0 0.2em 1.2em rgba(61,48,41,0.1);
+}
+.attribution {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background-color: #ea4c89;
+  color: #fff;
+  padding: 0.5em 1em;
+}
+
+
 </style>
